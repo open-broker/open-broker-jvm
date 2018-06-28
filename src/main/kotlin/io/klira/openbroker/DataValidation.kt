100b8cc4c5fd937ba7c0ef23ba2ae9a2daa72b5e
Anton Österberg <anton.osterberg@gmail.com>
2018-06-28 21:13:18 +0200

    Fix logical error in require predicate.

--- src/main/kotlin/io/klira/openbroker/DataValidation.kt
+++ src/main/kotlin/io/klira/openbroker/DataValidation.kt
@@ -3,7 +3,7 @@ package io.klira.openbroker
 internal fun Int?.requireMin(min: Int, propertyName: String? = null) {
     if(this == null)
         return
-    require(this < min) {
+    require(this >= min) {
         val descriptor: String = propertyName?.let { " '$it'" } ?: ""
         "Value$descriptor must be at least $min, but was $this"
     }
