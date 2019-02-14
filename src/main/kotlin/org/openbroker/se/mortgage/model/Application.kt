2b75d94bad95113c9b2ab2f39fad3cc35eba0f89
Anton Österberg <anton.osterberg@gmail.com>
2019-02-14 18:31:31 +0100

    Application: Add property adultsInHousehold.

--- src/main/kotlin/org/openbroker/se/mortgage/model/Application.kt
+++ src/main/kotlin/org/openbroker/se/mortgage/model/Application.kt
@@ -1,10 +1,17 @@
 package org.openbroker.se.mortgage.model
 
+import org.openbroker.common.requireMin
+
 data class Application @JvmOverloads constructor(
     val applicant: Applicant,
     val coApplicant: Applicant? = null,
     val refinancingProperty: RefinancingProperty,
+    val adultsInHousehold: Int? = null,
     val ownedProperties: List<OwnedProperty> = emptyList(),
     val existingLoans: List<ExistingLoan> = emptyList(),
     val extensions: Map<String, Any> = emptyMap()
-)
+) {
+    init {
+        adultsInHousehold.requireMin(1, "adultsInHouseHold")
+    }
+}
