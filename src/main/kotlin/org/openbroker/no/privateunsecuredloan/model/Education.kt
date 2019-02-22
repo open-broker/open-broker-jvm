c5abeb00d06999e7ec54de3273178c8ba86817ff
Beatrice Wittgren <bea@klira.io>
2019-02-22 14:08:37 +0100

    Adds models for norway privat eunsecured loan [Education, Responibility]

---
+++ src/main/kotlin/org/openbroker/no/privateunsecuredloan/model/Education.kt
@@ -0,0 +1,20 @@
+package org.openbroker.no.privateunsecuredloan.model
+
+enum class Education {
+    /**
+     * Completed the obligatory first stage of formal education
+     */
+    PRIMARY_SCHOOL,
+    /**
+     * Completed the formal education preparing for higher studies
+     */
+    SECONDARY_SCHOOL,
+    /**
+     * Completed 1-3 years of higher education
+     */
+    UNIVERSITY_SHORT,
+    /**
+     * Completed 4 or more years of higher education
+     */
+    UNIVERSITY_LONG
+}
