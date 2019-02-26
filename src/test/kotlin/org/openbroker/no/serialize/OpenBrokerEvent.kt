6c93754a11b25fcdda158747bfaf3b3ebd321d5c
Beatrice Wittgren <bea@klira.io>
2019-02-26 09:31:19 +0100

    Adds serialization tests

--- src/test/kotlin/org/openbroker/no/serialize/OpenBrokerEvent.kt
+++ src/test/kotlin/org/openbroker/no/serialize/OpenBrokerEvent.kt
@@ -1,2 +1,48 @@
 package org.openbroker.no.serialize
 
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.openbroker.no.privateunsecuredloan.TestObjectsJson
+import org.openbroker.cloudevents.CloudEvent
+import org.openbroker.cloudevents.cloudEvent
+import org.openbroker.common.OpenBrokerEvent
+import org.openbroker.common.serialize.restoreOpenBrokerEvent
+import org.openbroker.no.privateunsecuredloan.events.ApplicationCreated
+
+class OpenBrokerEvent {
+
+    @Test
+    fun testCloudEventWithSameData() {
+        val event: CloudEvent<ApplicationCreated> = cloudEvent(TestObjectsJson.fullApplicationCreatedJson)
+        val eventErasedType: CloudEvent<*> = event.copy()
+        val eventRestoredType: CloudEvent<OpenBrokerEvent>? = restoreOpenBrokerEvent(eventErasedType)
+        assertNotNull(eventRestoredType)
+        assertEquals(eventRestoredType, event)
+    }
+
+    @Test
+    fun testCloudEventWithErasedTypeViaJacksonData() {
+        val event: CloudEvent<ApplicationCreated> = cloudEvent(TestObjectsJson.fullApplicationCreatedJson)
+        val serialized: String = jacksonObjectMapper().writeValueAsString(event)
+        val deserialized: CloudEvent<*> = jacksonObjectMapper().readValue(serialized)
+        val eventRestoredType: CloudEvent<OpenBrokerEvent>? = restoreOpenBrokerEvent(deserialized)
+        assertNotNull(eventRestoredType)
+        assertEquals(eventRestoredType, event)
+    }
+
+    @Test
+    fun testCloudEventWithWrongType() {
+        val event: CloudEvent<String> = CloudEvent(eventType = "SomeType", data = "Hello", source = "test")
+        assertThrows<IllegalArgumentException> { restoreOpenBrokerEvent(event) }
+    }
+
+    @Test
+    fun testCloudEventWithNullData() {
+        val event: CloudEvent<String> = CloudEvent(eventType = "SomeType", data = null, source = "test")
+        val eventRestoredType: CloudEvent<OpenBrokerEvent>? = restoreOpenBrokerEvent(event)
+        assertNull(eventRestoredType)
+    }
+}
