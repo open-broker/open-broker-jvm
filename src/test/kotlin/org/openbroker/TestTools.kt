61f3de4ac53a19e644e6c90adba86852c3b6f685
Anton Österberg <anton.osterberg@gmail.com>
2019-05-02 16:24:36 +0200

    Add JSON example data for use in test cases and test case for Mortgage NO

--- src/test/kotlin/org/openbroker/TestTools.kt
+++ src/test/kotlin/org/openbroker/TestTools.kt
@@ -1,2 +1,18 @@
 package org.openbroker
 
+import org.openbroker.cloudevents.CloudEvent
+import org.openbroker.common.OpenBrokerEvent
+import org.openbroker.common.serialize.parseJsonCloudEvent
+import java.io.File
+
+private const val JSON_PATH = "src/test/resources/examples"
+
+internal fun loadJson(file: String): String {
+    val fileWithExtension: String = if(file.endsWith(".json")) file else "$file.json"
+    val completeFile = File("$JSON_PATH/$fileWithExtension")
+    require(completeFile.exists()) { "File cannot be found: $completeFile" }
+    return completeFile.readText()
+}
+
+internal inline fun <reified T: OpenBrokerEvent> parseJsonFromFile(file: String): CloudEvent<T> =
+    parseJsonCloudEvent(loadJson(file))
