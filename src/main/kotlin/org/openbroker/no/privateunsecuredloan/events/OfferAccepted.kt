ee70071046f0170e8455bddabedb7459998444f9
ElenaKurganova <elena@klira.io>
2021-01-25 14:14:41 +0100

    Add 'ssn' and 'ssnCoapplicant' to OfferAccepted

--- src/main/kotlin/org/openbroker/no/privateunsecuredloan/events/OfferAccepted.kt
+++ src/main/kotlin/org/openbroker/no/privateunsecuredloan/events/OfferAccepted.kt
@@ -1,6 +1,7 @@
 package org.openbroker.no.privateunsecuredloan.events
 
 import org.openbroker.common.model.Reference
+import org.openbroker.common.obfuscateDigits
 import org.openbroker.common.requireMin
 
 data class OfferAccepted @JvmOverloads constructor(
@@ -8,11 +9,25 @@ data class OfferAccepted @JvmOverloads constructor(
     val offerId: Reference? = null,
     val bankAccount: String? = null,
     val requestedCredit: Int? = null,
+    val ssn: String? = null,
+    val ssnCoapplicant: String? = null,
     val emailAddress: String? = null,
     val emailAddressCoapplicant: String? = null
-): PrivateUnsecuredLoanEvent
-{
+) : PrivateUnsecuredLoanEvent {
     init {
         requestedCredit.requireMin(1, "requestedCredit")
+
+        val ssnRegex = Regex("^[0-9]{11}$")
+        ssn?.let {
+            require(it.matches(ssnRegex)) {
+                "Invalid applicant's SSN: '${obfuscateDigits(it)}'"
+            }
+        }
+        ssnCoapplicant?.let {
+            require(it.matches(ssnRegex)) {
+                "Invalid co-applicant's SSN: '${obfuscateDigits(it)}'"
+            }
+        }
+
     }
 }
