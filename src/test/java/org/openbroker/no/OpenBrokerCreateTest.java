8fafe44b82b81b72bd08a78061140439aa90f877
ElenaKurganova <elena@klira.io>
2022-03-23 20:58:36 +0100

    Add property "termMonths" to OfferAccepted event for SE and NO

--- src/test/java/org/openbroker/no/OpenBrokerCreateTest.java
+++ src/test/java/org/openbroker/no/OpenBrokerCreateTest.java
@@ -6,6 +6,8 @@ import org.openbroker.common.OpenBrokerEventKt;
 import org.openbroker.common.model.Reference;
 import org.openbroker.no.privateunsecuredloan.events.OfferAccepted;
 
+import java.util.Objects;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class OpenBrokerCreateTest {
@@ -16,12 +18,15 @@ public class OpenBrokerCreateTest {
             new OfferAccepted(
                 new Reference("1", "io.klira"),
                 new Reference("123", "com.creditor"),
-                "12345678901"
+                "12345678901",
+                null,
+                56
             ),
             OfferAccepted.class,
             "source"
         );
 
         assertEquals("12345678901", accept.getData().getBankAccount());
+        assertEquals(56, Objects.requireNonNull(accept.getData().getTermMonths()).intValue());
     }
 }
--- src/test/java/org/openbroker/se/OpenBrokerCreateTest.java
+++ src/test/java/org/openbroker/se/OpenBrokerCreateTest.java
@@ -7,6 +7,8 @@ import org.openbroker.se.model.BankAccount;
 import org.openbroker.common.model.Reference;
 import org.openbroker.se.privateunsecuredloan.events.OfferAccepted;
 
+import java.util.Objects;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class OpenBrokerCreateTest {
@@ -17,12 +19,15 @@ public class OpenBrokerCreateTest {
             new OfferAccepted(
                 new Reference("1", "io.klira"),
                 new Reference("2", "com.creditor"),
-                new BankAccount("3300", "1234567890", "Nordea")
+                new BankAccount("3300", "1234567890", "Nordea"),
+                null,
+                84
             ),
             OfferAccepted.class,
             "source"
         );
 
         assertEquals("3300", accept.getData().getBankAccount().getClearingNo());
+        assertEquals(84, Objects.requireNonNull(accept.getData().getTermMonths()).intValue());
     }
 }
