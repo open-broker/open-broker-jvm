d1faa8a159862fd33decc4bdc5ffedc492dd75d2
Ilya Komlev <ilya.komlev@zensum.se>
2024-02-29 19:21:16 +0300

    Make tentativeAddress.address, tentativeAddress.city, tentativeAddress.postalCode optional (NO)

--- src/main/kotlin/org/openbroker/no/model/Address.kt
+++ src/main/kotlin/org/openbroker/no/model/Address.kt
@@ -6,21 +6,19 @@ import org.openbroker.common.requireNotBlank
 data class Address(
     val firstName: String,
     val lastName: String,
-    val address: String,
-    val city: String,
-    val postalCode: String,
+    val address: String? = null,
+    val city: String? = null,
+    val postalCode: String? = null,
     val careOf: String? = null
 ) {
     init {
         firstName.requireNotBlank("firstName")
         lastName.requireNotBlank("lastName")
-        address.requireNotBlank("address")
-        city.requireNotBlank("city")
-        postalCode.requireMatchRegex(postalCodeRegex, "postalCode")
+        postalCode?.requireMatchRegex(postalCodeRegex, "postalCode")
         careOf?.requireNotBlank("careOf")
     }
 
     companion object {
-        val postalCodeRegex = Regex("^\\d{4}$")
+        val postalCodeRegex = Regex("^(\\d{4})?$")
     }
 }
