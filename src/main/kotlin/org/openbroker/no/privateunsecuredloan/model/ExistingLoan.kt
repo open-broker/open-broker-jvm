678c264f4ece47af249b0c04a3e5c481af540f61
Sergey Holovach <sergey.holovach@zensum.se>
2025-08-14 11:14:28 +0300

    chore: add existingLoan.nominalInterestRate, existingLoan.remainingTerms (NO apps)

--- src/main/kotlin/org/openbroker/no/privateunsecuredloan/model/ExistingLoan.kt
+++ src/main/kotlin/org/openbroker/no/privateunsecuredloan/model/ExistingLoan.kt
@@ -1,9 +1,12 @@
 package org.openbroker.no.privateunsecuredloan.model
 
+import org.openbroker.common.requireMatchRegex
 import org.openbroker.common.requireMin
 
 data class ExistingLoan(
     val loanAmount: Int,
+    val nominalInterestRate: String? = null,
+    val remainingTerms: Int? = null,
     val monthlyPayment: Int,
     val refinanceAmount: Int? = null,
     val existingLoanType: ExistingLoanType,
@@ -12,6 +15,9 @@ data class ExistingLoan(
 ) {
     init {
         loanAmount.requireMin(1, "loanAmount")
+        val interestRateRgx = Regex("^(0\\.[0-9]+|1\\.0+)$")
+        nominalInterestRate?.requireMatchRegex(interestRateRgx, "nominalInterestRate")
+        remainingTerms?.requireMin(1, "remainingTerms")
         monthlyPayment.requireMin(1, "monthlyPayment")
     }
 }
